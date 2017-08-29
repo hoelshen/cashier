22bbbd20cd7de2cd32109a43821c6e8a47c38ab6
fangliying <fangliying@zuipin.cn>
2017-08-29 11:33:58 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,10 @@
 import slideBar from './components/slideBar.vue';
 import mainWrap from './components/mainWrap.vue';
 export default {
-	data () {
-	    return {
-		    msg: 'Want something new?'
-		}
-	},
 	components :{
 		slideBar,
 		mainWrap
 	},
-	methods: {
-		startHacking () {
-			this.$notify({
-				title: 'Shhh',
-				message: 'Just be patient...',
-				duration: 6000
-			})
-		}
-	}
 }
 </script>
 
--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ Vue.use(Router);
 
 export default new Router({
     routes : [
-        {path: '/',component: require('./App.vue')},
+        {path: '/',component: resolve => require(['./App.vue'], resolve)},
         { path: '/login', component: resolve => require(['./views/Login.vue'], resolve) }
     ]
 })
