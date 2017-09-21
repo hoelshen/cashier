00dd4f54ef494ea523a31ac12cef47e82ecc28d6
xiebaota <xiebaota@zuipin.control.com>
2017-09-21 17:20:25 +0800

    0921退款单管理01

--- src/router.js
+++ src/router.js
@@ -3,48 +3,64 @@ import Router from 'vue-router';
 
 Vue.use(Router);
 
-const config = [
-	{
+const config = [{
 		path: '/login',
 		component: resolve => require(['./views/Login.vue'], resolve)
 	},
 	{
 		path: '/',
 		component: resolve => require(['./components/mainWrap.vue'], resolve),
-		children:[
-			{path: 'testTree',component: resolve => require(['./views/testTree.vue'], resolve)},
-			{
-				path:'orderManage',
-				component:resolve=>require(['./views/orderManage.vue'],resolve),
+		children: [{
+				path: 'testTree',
+				component: resolve => require(['./views/testTree.vue'], resolve)
 			},
 			{
-				path:'waitOrder',
-				component:resolve=>require(['./views/waitOrder.vue'],resolve),
+				path: 'orderManage',
+				component: resolve => require(['./views/orderManage.vue'], resolve),
 			},
 			{
-				path:'prepaidManage',
-				component:resolve=>require(['./views/prepaidManage.vue'],resolve),
+				path: 'waitOrder',
+				component: resolve => require(['./views/waitOrder.vue'], resolve),
 			},
 			{
-				path:'prepaidManage/:shopNo',
-				name:'prepaidManage',
-				component:resolve=>require(['./views/prepaidManage.vue'],resolve),
+				path: 'prepaidManage',
+				component: resolve => require(['./views/prepaidManage.vue'], resolve),
 			},
 			{
-				path:'memberList',
-				component:resolve=>require(['./views/memberList.vue'],resolve),
+				path: 'drawBack',
+				component: resolve => require(['./views/drawBack.vue'], resolve),
 			},
-			{ path: 'storeManage', component: resolve => require(['./views/storeManage.vue'], resolve) },
 			{
-				path:'orderInfo/:purchaseOrderNo/:shopNo',
-				name:'orderInfo',
-				component:resolve=>require(['./views/orderInfo.vue'],resolve),
+				path: 'waitdrawBack',
+				component: resolve => require(['./views/waitdrawBack.vue'], resolve),
+			},
+			{
+				path: 'drawBackDetail',
+				component: resolve => require(['./views/drawBackDetail.vue'], resolve),
+			},
+			{
+				path: 'prepaidManage/:shopNo',
+				name: 'prepaidManage',
+				component: resolve => require(['./views/prepaidManage.vue'], resolve),
+			},
+			{
+				path: 'memberList',
+				component: resolve => require(['./views/memberList.vue'], resolve),
+			},
+			{
+				path: 'storeManage',
+				component: resolve => require(['./views/storeManage.vue'], resolve)
+			},
+			{
+				path: 'orderInfo/:purchaseOrderNo/:shopNo',
+				name: 'orderInfo',
+				component: resolve => require(['./views/orderInfo.vue'], resolve),
 			},
 		]
 	},
 ]
 
 const router = new Router({
-    routes: config
+	routes: config
 })
 export default router;
