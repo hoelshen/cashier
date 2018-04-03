110c04f6cbe414dc295e67f1843d946a5872ab13
陈俊杰 <chenjunjie@zuipin.cn>
2018-04-03 21:53:25 +0800

    更新 editor.vue

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -219,7 +219,7 @@ export default {
                         accessKeyId: res.data.data.accessKeyId,
                         accessKeySecret: res.data.data.accessKeySecret,
                         stsToken: res.data.data.securityToken,
-                        bucket: 'imgzuipin'
+                        bucket: 'cashier-img'
                     });
 
                     client.multipartUpload('/cashierImg/' + this.formatDate(new Date()).replace(/\s|\-|\:/g,'') + '.' + this.$refs.imgBtn.files[0].name.split('.').pop(), this.$refs.imgBtn.files[0]).then(res => {
