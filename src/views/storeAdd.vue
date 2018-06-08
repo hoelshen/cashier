609b7819b27dabe644d7cb882a4a5205498c85fe
huangchunhong <1358191801@qq.com>
2018-06-08 15:23:15 +0800

    店铺新增时间控件

--- src/views/storeAdd.vue
+++ src/views/storeAdd.vue
@@ -371,7 +371,7 @@ export default {
                disabledDate(time) {
                    let now = new Date();
                    let startDate = new Date().setDate(new Date().getDate() - 31)
-                   let endDate = new Date().setDate(new Date().getDate() + 30)
+                   let endDate = new Date().setDate(new Date().getDate() + 29)
                    return time.getTime() > endDate || time.getTime() < startDate;
                 }
             },
